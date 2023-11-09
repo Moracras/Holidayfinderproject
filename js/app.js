b1b7b593bcf3c6ed19afca89d43e673daa4d875b
Moracras <tavfikkarademir@gmail.com>
2023-11-09 23:55:47 +0300

    last step

---
+++ js/app.js
@@ -0,0 +1,8 @@
+
+
+const hamburgerBtn = document.querySelector(".fa-bars")
+
+hamburgerBtn.addEventListener("click", () => {
+  const headerLinks = document.querySelector(".header__links")
+  headerLinks.classList.toggle("show")
+})
